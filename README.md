4e471e4983856bec0cc7b2131c0f22832a714008
Dongwoon Son <dplusic@gmail.com>
2019-08-10 14:26:53 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# web-vr-hello-planiverse
+
+## Reference
+* https://github.com/GoogleChrome/samples/tree/gh-pages/web-vr/hello-world
---
+++ src/demo.ts
@@ -0,0 +1,157 @@
+/*!
+ *
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as THREE from 'three'
+import * as THREE_TEXT2D from 'three-text2d'
+
+type Settings = {
+  viewAngle: number
+  near: number
+  far: number
+}
+
+export class Demo {
+  _width: number | null = null
+  _height: number | null = null
+  _renderer: THREE.WebGLRenderer | null = null
+  _camera: THREE.PerspectiveCamera | null = null
+  _aspect: number | null = null
+  _settings: Settings | null = null
+  _box1: THREE.Mesh | null = null
+  _box2: THREE.Mesh | null = null
+  _container: Element | null = null
+  _scene: THREE.Scene | null = null
+
+  static get CAMERA_SETTINGS() {
+    return {
+      viewAngle: 45,
+      near: 0.1,
+      far: 10000,
+    }
+  }
+
+  constructor() {
+    this._container = document.querySelector('#container')
+
+    this.clearContainer()
+    this.createRenderer()
+
+    this._onResize = this._onResize.bind(this)
+    this._update = this._update.bind(this)
+    this._onResize()
+
+    this.createCamera()
+    this.createScene()
+    this.createMeshes()
+    this.createTexts()
+
+    this._addEventListeners()
+    requestAnimationFrame(this._update)
+  }
+
+  _update() {
+    const ROTATION_VALUE = 4
+    const time = window.performance.now() * 0.0001
+
+    this._box1!.rotation.x = Math.sin(time) * ROTATION_VALUE
+    // this._box1!.rotation.y = Math.cos(time) * ROTATION_VALUE
+
+    this._box2!.rotation.x = Math.sin(time) * ROTATION_VALUE
+    // this._box2!.rotation.y = Math.cos(time) * ROTATION_VALUE
+
+    this._render()
+  }
+
+  _render() {
+    this._renderer!.render(this._scene!, this._camera!)
+    requestAnimationFrame(this._update)
+  }
+
+  _onResize() {
+    this._width = window.innerWidth
+    this._height = window.innerHeight
+    this._aspect = this._width / this._height
+
+    this._renderer!.setPixelRatio(window.devicePixelRatio)
+    this._renderer!.setSize(this._width, this._height)
+
+    if (!this._camera) {
+      return
+    }
+
+    this._camera.aspect = this._aspect
+    this._camera.updateProjectionMatrix()
+  }
+
+  _addEventListeners() {
+    window.addEventListener('resize', this._onResize)
+  }
+
+  clearContainer() {
+    this._container!.innerHTML = ''
+  }
+
+  createRenderer() {
+    this._renderer = new THREE.WebGLRenderer()
+    this._container!.appendChild(this._renderer.domElement)
+  }
+
+  createCamera() {
+    this._settings = Demo.CAMERA_SETTINGS
+    this._camera = new THREE.PerspectiveCamera(
+      this._settings.viewAngle,
+      this._aspect!,
+      this._settings.near,
+      this._settings.far,
+    )
+  }
+
+  createScene() {
+    this._scene = new THREE.Scene()
+  }
+
+  createMeshes() {
+    // Box1
+    this._box1 = new THREE.Mesh(new THREE.BoxGeometry(0.03, 1, 1), new THREE.MeshNormalMaterial())
+    this._box1.position.y = 1.5
+    this._box1.position.z = -7
+
+    // Box2
+    this._box2 = new THREE.Mesh(new THREE.BoxGeometry(0.1, 3, 3), new THREE.MeshNormalMaterial())
+    this._box2.position.y = -1
+    this._box2.position.z = -21
+
+    this._scene!.add(this._box1)
+    this._scene!.add(this._box2)
+  }
+
+  createTexts() {
+    const text1 = new THREE_TEXT2D.SpriteText2D('Which one is farther away?', {
+      align: THREE_TEXT2D.textAlign.center,
+      font: '100px Arial',
+      fillStyle: '#FFFFFF',
+      antialias: false,
+    })
+    text1.position.x = 0
+    text1.position.y = -1
+    text1.position.z = -4
+    text1.scale.x = 0.002
+    text1.scale.y = 0.002
+
+    this._scene!.add(text1)
+  }
+}
